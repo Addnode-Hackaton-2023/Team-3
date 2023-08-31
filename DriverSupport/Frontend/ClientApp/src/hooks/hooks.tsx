b19727a567815e9a46c85275d97daeea0808e99c
Krister Wicksell <krister.wicksell@sokigo.com>
2023-08-31 13:02:21 +0200

    Fix build issue

--- DriverSupport/Frontend/ClientApp/src/hooks/hooks.tsx
+++ DriverSupport/Frontend/ClientApp/src/hooks/hooks.tsx
@@ -11,7 +11,6 @@ import Circle from "@arcgis/core/geometry/Circle";
 import Graphic from "@arcgis/core/Graphic";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import Collection from "@arcgis/core/core/Collection";
-import esriConfig from '@arcgis/core/config';
 
 export const APIKEY = ""
 
@@ -84,7 +83,7 @@ const createLayer = (stops: Collection<IDrivingStop>, color: string, name: strin
           family: "CalciteWebCoreIcons"
         }
       }
-    }
+    } as any
   });
 }
 
@@ -117,7 +116,7 @@ const createLayerWithoutPopups = (stops?: IDrivingStop[], color?: string, name?:
           family: "CalciteWebCoreIcons"
         }
       }
-    }
+    } as any
   });
 }
 
