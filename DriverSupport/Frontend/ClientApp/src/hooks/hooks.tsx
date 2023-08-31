24dcde5d688e1696d454a82c7f92d6249086ac4c
Jonas Lindemann <jonas.lindemann@symetri.com>
2023-08-31 12:13:05 +0200

    Added popup

--- DriverSupport/Frontend/ClientApp/src/hooks/hooks.tsx
+++ DriverSupport/Frontend/ClientApp/src/hooks/hooks.tsx
@@ -11,6 +11,7 @@ import Circle from "@arcgis/core/geometry/Circle";
 import Graphic from "@arcgis/core/Graphic";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import Collection from "@arcgis/core/core/Collection";
+import PopupTemplate from "@arcgis/core/PopupTemplate";
 
 const createStops = (drivingStops?: IDrivingStop[]) => {
 
@@ -19,19 +20,56 @@ const createStops = (drivingStops?: IDrivingStop[]) => {
   );
 }
 
-const createLayer = (graphics: Collection<Graphic>, color: string, name: string, symbol: string) => {
+const createLayer = (stops: Collection<IDrivingStop>, color: string, name: string, symbol: string) => {
+  var graphics = new Collection<Graphic>(stops.map((s, index) => new Graphic({
+    geometry: new Circle({
+      center: s.stop.point,
+      radius: 100,
+      radiusUnit: "meters"
+    }),
+    attributes: {
+      "ObjectID": index,
+      "name": s.stop.name,
+      "contact": s.stop.contact,
+      "address": s.stop.address
+    }
+  })));
   return new FeatureLayer({
     source: graphics,
     objectIdField: name,
     fields: [{
-      name: name,
-      type: "oid"
-    }, {
-      name: "url",
+      name: "ObjectID",
+      type: "oid",
+    },
+    {
+      name: "name",
+      type: "string"
+    },
+    {
+      name: "contact",
+      type: "string"
+    },
+    {
+      name: "address",
       type: "string"
     }],
     popupTemplate: {
-      content: "<img src='{url}'>"
+      title: "{name}",
+      content: [
+        {
+          type: "fields",
+          fieldInfos: [
+            {
+              fieldName: "contact",
+              label: "Contact:"
+            },
+            {
+              fieldName: "address",
+              label: "Address:"
+            }
+          ]
+        }
+      ]
     },
     renderer: {  // overrides the layer's default renderer
       type: "simple",
@@ -57,28 +95,20 @@ export const useCreateMap = (mapRef: MutableRefObject<HTMLDivElement | null>, dr
     let view: MapView;
     const routePolyline = Polyline.fromJSON(driving?.routePolyline);
     const initializeMap = async (mapRef: MutableRefObject<string | HTMLDivElement | null>, routeLayer: RouteLayer, drivingStops?: IDrivingStop[]) => {
-      const visited = new Collection<Graphic>();
-      const notVisited = new Collection<Graphic>();
+      const visited = new Collection<IDrivingStop>();
+      const notVisited = new Collection<IDrivingStop>();
       drivingStops?.forEach(s => {
         if (s.stop.type === 1)
         {
-          var graphics = new Graphic({
-            geometry: new Circle({
-              center: s.stop.point,
-              radius: 100,
-              radiusUnit: "meters"
-            })
-          })
-          
           if (s.weight == null)
-            notVisited.add(graphics);
+            notVisited.add(s);
           else
-            visited.add(graphics);
+            visited.add(s);
         }
       });
 
       const visitedLayer = createLayer(visited, "#0AEB30", "visited", "\ue612");
-      const notVisitedLayer = createLayer(notVisited, "red", "not visited", "\ue613");
+      const notVisitedLayer = createLayer(notVisited, "#EB3F0A", "not visited", "\ue613");
       console.log("visited: " + visited.length);
       console.log("not visited: " + notVisited.length);
       const map = new Map({ basemap: 'satellite', layers: [routeLayer, visitedLayer, notVisitedLayer]});
